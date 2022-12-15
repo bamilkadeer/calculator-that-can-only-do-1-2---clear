a738b58ad6f123bb5d28e609b82bf99d730ce118
bamilkadeer <119720173+bamilkadeer@users.noreply.github.com>
2022-12-14 19:28:58 -0800

    evolved

--- script.js
+++ script.js
@@ -1,126 +1,125 @@
 const number =  document.querySelectorAll('[data-number]');
-const clear =  document.querySelectorAll('[data-clear]');
+const clear  =  document.querySelectorAll('[data-clear]');
 const symbol =  document.querySelectorAll('[data-symbol]');
-let screen =  document.querySelector('.number');
-const equals = document.querySelectorAll('[data-equals]');
+const equals =  document.querySelectorAll('[data-equals]');
+let   screen =  document.querySelector   ('.number');
+let preNumber = document.querySelector   ('.previousNumber');
 
-let counter = 0;
+
+
+let counter  = 0;
+let lastchar = true; 
+let buffer   = '';
+let bufferbuffer = '';
 let lastSymbol;
 let firstInput;
 let secondInput;
 let runningTotal;
-let lastchar = true; 
-let buffer = '';
-//const number =  document.querySelector('.one');
-
-
-
-
-
-
-    
-
 
 number.forEach(function(button) {
 
+    button.onclick = function()
+    {
+     
+     screen.innerText = buffer;
+     screen.style.color = 'black';
+     lastchar = button.innerText.toString();
+     buffer = buffer.toString() + button.innerText.toString();
+     bufferbuffer = bufferbuffer.toString() + button.innerText.toString();
 
-    button.onclick = function(){
-        screen.innerText = buffer;
-
-        screen.style.color = 'black';
-        lastchar = button.innerText.toString();
-        buffer = buffer.toString() + button.innerText.toString();
-screen.innerText = buffer;
+     screen.innerText = buffer;
     }
-    
 });
-
 
 clear.forEach(function(button) {
 
-
-
-    button.onclick = function(){
-        counter = 0;
-        screen.style.color = 'green';
-        buffer = '';
-screen.innerText = buffer;
+    button.onclick = function()
+    {
+     counter = 0;
+     preNumber.innerText = '';
+     screen.style.color = 'green';
+     buffer = '';
+     bufferbuffer = '';
+     screen.innerText = buffer;
     }
-    
 });
 
 symbol.forEach(function(button) {
 
-
-
-    button.onclick = function(){
-
-        if(screen.innerText === '' || lastchar === false || counter >= 1){
- return;
-
-        }
-        else{
+    button.onclick = function()
+    {
+     if(screen.innerText === '' || lastchar === false || counter >= 1)
+       { 
+        return;
+       }
+       else{
             lastSymbol = button.innerText.toString();
             counter++;
             screen.style.color = 'green';
             buffer = buffer.toString() + button.innerText.toString();
-    screen.innerText = buffer;
-    lastchar = false;
-    firstInput = parseInt(buffer);
-    buffer = '';
-console.log(firstInput);
-        }
+
+            bufferbuffer = bufferbuffer.toString() + button.innerText.toString();
+
+            console.log(buffer);
+            preNumber.innerText = bufferbuffer;
+
+            screen.innerText = buffer;
+            lastchar = false;
+            firstInput = parseInt(buffer);
+            buffer = '';
+            bufferbuffer = '';
+            console.log(firstInput);
+           }
     }
-    
 });
 
+equals.forEach(function(button){
+
+    button.onclick = function()
+    {
+    counter = 0;
+    preNumber.innerText = buffer;
+
+    calculate(firstInput, parseInt(buffer));
+
+
+    console.log (parseInt(bufferbuffer));
+    firstInput = parseInt(bufferbuffer);
+    screen.style.color = 'red';
 
 
 
-equals.forEach(function(button) {
 
-
-    button.onclick = function(){
-counter = 0;
-
-calculate(firstInput, parseInt(buffer));
-
-
-        screen.style.color = 'red';
     }
-    
 });
-
-
 
 function calculate (x, y){
 
-
     console.log(symbol.innerText)
     console.log(lastSymbol)
-switch(lastSymbol){
-   
-   case "-":
-   
-    buffer =  x - y;
-   console.log(x);
-   console.log(y);
+    switch(lastSymbol){
+                       case "-":
+                        buffer =  x - y;
+                        screen.innerText = buffer.toString();
+                        
+                       break;
+     
+                       case "+":
+                        buffer =  x + y;
+                        screen.innerText = buffer.toString();
 
-   console.log(buffer);
-   screen.innerText = buffer.toString();
-   console.log('hello');
-break;
+                       break;
 
-case "+":
+                       case "÷":
+                        buffer =  x /y;
+                        screen.innerText = buffer.toString();
+                        
+                       break;
+     
+                       case "x":
+                        buffer =  x * y;
+                        screen.innerText = buffer.toString();
 
-buffer =  x + y;
-console.log(x);
-console.log(y);
-
-console.log(buffer);
-screen.innerText = buffer.toString();
-console.log('hello');
-break;
-
-}
+                       break;
+                      }
 }
